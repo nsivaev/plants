8ed808cefdf59d254ca1d6286e72b47fbe52779a
nsivaev <nsivaev@list.ru>
2023-01-19 09:30:20 +0300

    feat: fix menu-burger

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let menuBtn = document.getElementById('#menu-btn');
-let menu = document.querySelector('.menu');
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', handleClick)
 menu.addEventListener('click', function(){
     menu.classList.toggle('active');
 })
 
 function handleClick() {
-    console.log('123')
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
 }
