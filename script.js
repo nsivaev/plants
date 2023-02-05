4c6e9d1fb2e8c1800f8a2d11f262314bb31e04c2
nsivaev <nsivaev@list.ru>
2023-02-05 23:33:21 +0300

    feat: fix service

--- script.js
+++ script.js
@@ -6,8 +6,48 @@ const menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', handleClick)
 menu.addEventListener('click', handleClick)
 
+
 // functions
 function handleClick() {
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
 }
+
+
+//service
+const gardenBtn = document.getElementById('buttonGardens');
+const lawnBtn = document.getElementById('buttonLawn');
+const plantingBtn = document.getElementById('buttonPlanting');
+
+const gardenCard = document.querySelectorAll('.garden_card');
+const lawnCard = document.querySelectorAll('.lawn_card');
+const plantingCard = document.querySelectorAll('.planting_card');
+
+
+gardenBtn.addEventListener('click', function () {
+    plantingCard.forEach(function (item) {
+        item.classList.toggle('selected');
+    });
+    lawnCard.forEach(function (item) {
+        item.classList.toggle('selected');
+    });
+});
+
+plantingBtn.addEventListener('click', function () {
+    gardenCard.forEach(function (item) {
+        item.classList.toggle('selected');
+    });
+    lawnCard.forEach(function (item) {
+        item.classList.toggle('selected');
+    });
+
+});
+
+lawnBtn.addEventListener('click', function () {
+    plantingCard.forEach(function (item) {
+        item.classList.toggle('selected');
+    });
+    gardenCard.forEach(function (item) {
+        item.classList.toggle('selected');
+    });
+});
